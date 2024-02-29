34f41179bced647c08632c3dc18a0dc99ef7dba6
ridhdhy rajpara <rajpararidhdhy@gmail.com>
2024-02-29 21:59:23 +0530

    changes2

--- Index.js
+++ Index.js
@@ -11,16 +11,15 @@ dotenv.config();
 app.use (express.json({limit: "30mb", extended:true}))
 app.use(express.urlencoded({limit: "30mb", extended:true}))
 app.use(cors());
-
 app.get('/',(req, res) => {
     res.send("This is a stack overflow clone API")
    })
-
    app.use("/user", userRoutes);
    app.use("/questions", questionRoutes);
    app.use("/answer", answerRoutes);
  const PORT = process.env.PORT || 5000;
-   const CONNECTION_URL = "mongodb+srv://rajpararidhdhy:5P0fi1mrKqWgV867@stak9.1quzk6h.mongodb.net/stak9?retryWrites=true&w=majority";
+  //  const CONNECTION_URL = "mongodb+srv://rajpararidhdhy:5P0fi1mrKqWgV867@stak9.1quzk6h.mongodb.net/stak9?retryWrites=true&w=majority";
+ 
   //  const CONNECTION_URL = "mongodb+srv://rajpararidhdhy:5P0fi1mrKqWgV867@stak9.1quzk6h.mongodb.net/stak9?retryWrites=true&w=majority";
                               // mongodb+srv://rajpararidhdhy:<password>@stak9.1quzk6h.mongodb.net/?retryWrites=true&w=majority&appName=stak9 
   const DATABASE_URL = process.env.CONNECTION_URL
